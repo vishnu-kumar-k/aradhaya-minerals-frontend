8e20399e2820143232a4946f2204fd8889a682c9
vishnu-kumar-k <vishnuk55265@gmail.com>
2023-04-19 21:47:18 +0530

    last

--- src/Component/Loading.js
+++ src/Component/Loading.js
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 import BarLoader from "react-spinners/SyncLoader";
-import "../css/Loading.css"
+import "../css/Loading.css";
 const Loading = () => {
   return (
     <div className="App d-flex flex-column">
-      
       <div className="flex-grow-1 d-flex align-items-center justify-content-center">
-        
-        <BarLoader  color="#0077FE"/>
+        <BarLoader color="#0077FE" />
       </div>
     </div>
-  )
-}
+  );
+};
 export default Loading;
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,82 +4,96 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link, useNavigate } from "react-router-dom";
 import "../css/Navbaradmin.css";
-import  axios  from "../Axios/Axios";
+import axios from "../Axios/Axios";
 import { useRecoilState } from "recoil";
-import { AdminDetails, Load, Width } from "../Atom/Atom";
+import { AdminDetails, Load, ShowModel, Width } from "../Atom/Atom";
 import Loading from "./Loading";
 const Menu = () => {
   const navigate = useNavigate();
-  const[admindetails,setAdmindetails]=useRecoilState(AdminDetails);
-  const[loading,setLoading]=useRecoilState(Load);
-  useEffect(()=>{
-
-    if(admindetails.jwt!==undefined && admindetails.jwt!==null)
-    {
+  const [admindetails, setAdmindetails] = useRecoilState(AdminDetails);
+  const [loading, setLoading] = useRecoilState(Load);
+  const [showModel, setShowModel] = useRecoilState(ShowModel);
+  useEffect(() => {
+    if (admindetails.jwt !== undefined && admindetails.jwt !== null) {
       setLoading(true);
-        axios.post("/admin/verify",{
-            jwt_token:admindetails.jwt
-        }).then(async(result)=>{
-          setLoading(false)
-            if(!result.data.status)
-            {
-                navigate("/admin/login");
-            }
-            else
-            {
-                setAdmindetails({status:true,name:result.data.name,jwt:admindetails.jwt});
-            }
-        }).catch((err)=>console.log(err));
+      axios
+        .post("/admin/verify", {
+          jwt_token: admindetails.jwt,
+        })
+        .then(async (result) => {
+          setLoading(false);
+          if (!result.data.status) {
+            navigate("/admin/login");
+          } else {
+            setAdmindetails({
+              status: true,
+              name: result.data.name,
+              jwt: admindetails.jwt,
+            });
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      navigate("/admin/login");
     }
-    else
-    {
-        navigate("/admin/login");
-    }
-  },[])
-
-
+  }, []);
 
   const handleLogout = (e) => {
     e.preventDefault();
     localStorage.removeItem("adminJwt");
     navigate("/admin/login");
   };
-  return (<>
-    {loading?(<></>):(
-    <Container>
-        <h1 className="admin">Admin Panel</h1>
-      <Navbar bg="light" expand="lg" className="fixed-top">
+  const handleFilter = (e) => {
+    e.preventDefault();
+    setShowModel(true);
+  };
+  return (
+    <>
+      {loading ? (
+        <></>
+      ) : (
         <Container>
-        <Navbar.Brand>Aradhaya minerals</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            
-            <Link to="/admin/addadmin" className="nav-link">
-              Add admin 
-            </Link>
-            <Link to="/admin/viewproduct" className="nav-link">
-              Can
-            </Link>
-            <Link to="/admin" className="nav-link">
-               Order
-            </Link>
-          </Nav>
-          <div className="d-flex gap-3">
-            <Link className="nav-link">{admindetails.name}</Link>{"  "}
-          
-            <Link className="nav-link" onClick={handleLogout}>
-              Logout
-            </Link>
-          </div>
-        </Navbar.Collapse>
+          <h1 className="admin">Admin Panel</h1>
+          <Navbar bg="light" expand="lg" className="fixed-top">
+            <Container>
+              <Navbar.Brand>Aradhaya minerals</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: "200px" }}
+                  navbarScroll
+                >
+                  <Link to="/admin/addadmin" className="nav-link">
+                    Admins
+                  </Link>
+                  <Link to="/admin/viewproduct" className="nav-link">
+                    Can
+                  </Link>
+                  <Link to="/admin" className="nav-link">
+                    Order
+                  </Link>
+                  <Link className="nav-link" onClick={handleFilter}>
+                    Filter
+                  </Link>
+                </Nav>
+
+                <div className="d-flex gap-3">
+                  <Nav>
+                    <Link className="nav-link">{admindetails.name}</Link>
+                    {"  "}
+
+                    <Link className="nav-link" onClick={handleLogout}>
+                      Logout
+                    </Link>
+                  </Nav>
+                </div>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
         </Container>
-      </Navbar>
-    </Container>)}</>
+      )}
+    </>
   );
 };
 
